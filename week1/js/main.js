b20322edea88f0b1dd4f9b5b21394f72cd46ee04
Carver <kerenketepela@gmail.com>
2022-02-04 11:02:22 +0300

    rm cusual dir and files

--- week1/js/main.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body");
-
-body.style.backgroundColor = "whitesmoke";
-
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", () => {
-  alert("Zetech Uni Web Course");
-});
